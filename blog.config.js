db27f677543fbde17b63049ae4d1ac2fedbf484e
Jakey <jakey_yang@hotmail.com>
2023-04-29 08:11:38 +0800

    feat: 修改博客作者名称

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '泥巴 Team', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '泥巴电报', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '爱运动的技术爱好者，ex Alipay iOSer；一些奇怪爱好：捡垃圾、Homelab、PVE、AI', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.ioser.store', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '泥巴电报, 博客', // 网站关键词 英文逗号隔开
